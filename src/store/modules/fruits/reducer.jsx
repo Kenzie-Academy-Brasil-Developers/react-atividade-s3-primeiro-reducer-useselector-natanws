1cda76baa7595cc7f658c7cad3e010d323efabcd
NatanWS <natanwsn@gmail.com>
2021-09-28 19:57:51 -0300

    corrigido caminho para imagens

--- src/store/modules/fruits/reducer.jsx
+++ src/store/modules/fruits/reducer.jsx
@@ -1,7 +1,11 @@
+import Banana from "../../../assets/img/banana.png";
+import Morango from "../../../assets/img/strawberry.png";
+import Maca from "../../../assets/img/apple.png";
+
 const initialState = [
-  { name: "Banana", image: "../../assets/img/banana.png" },
-  { name: "Maçã", image: "src/assets/img/apple.png" },
-  { name: "Morango", image: "src/assets/img/strawberry.png" },
+  { name: "Banana", image: Banana },
+  { name: "Maçã", image: Maca },
+  { name: "Morango", image: Morango },
 ];
 
 const fruitsReducer = (state = initialState, action) => {
